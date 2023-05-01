392ee11e386f77f2ae54ba1b41e6dac525dd84da
Alex Aandonesei <109587363+AlexCodeTitan@users.noreply.github.com>
2023-05-01 14:26:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppWrapper } from "./App.style";
+
+function App() {
+  return (
+    <AppWrapper>
+      <h1>Hello</h1>
+    </AppWrapper>
+  );
+}
+
+export default App;
